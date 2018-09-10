b505683ccbba250d478f37ae8af1a99e25da65fc
Alejandro Guirao <lekumberri@gmail.com>
2018-09-10 09:33:45 +0200

    Add test for car factory (abstract factory)

--- ch02/abstractFactory/vehicleFactory_test.go
+++ ch02/abstractFactory/vehicleFactory_test.go
@@ -26,3 +26,28 @@ func TestMotorbikeFactory(t *testing.T) {
 	t.Logf("Sport motorbike has type %d\n", sportBike.GetMotorbikeType())
 
 }
+
+func TestCarFactory(t *testing.T) {
+	carF, err := BuildFactory(CarFactoryType)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	carVehicle, err := carF.NewVehicle(LuxuryCarType)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Car vehicle has %d wheels\n", carVehicle.NumWheels())
+
+	luxuryCar, ok := carVehicle.(Car)
+
+	if !ok {
+		t.Fatal("Struct assertion has failed")
+	}
+
+	t.Logf("Luxury car has %d doors.\n", luxuryCar.NumDoors())
+
+}
