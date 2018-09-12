3dd16d226c726707eecd22db12ba092816995959
Alejandro Guirao <lekumberri@gmail.com>
2018-09-12 08:47:58 +0200

    Implement prototype pattern

--- ch02/prototype/prototype.go
+++ ch02/prototype/prototype.go
@@ -1,6 +1,9 @@
 package prototype
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type ShirtCloner interface {
 	GetClone(s int) (ItemInfoGetter, error)
@@ -19,7 +22,19 @@ func GetShirtsCloner() ShirtCloner {
 type ShirtsCache struct{}
 
 func (i *ShirtsCache) GetClone(s int) (ItemInfoGetter, error) {
-	return nil, errors.New("Not implemented")
+	var newShirt Shirt
+	switch s {
+	case White:
+		newShirt = *whitePrototype
+	case Black:
+		newShirt = *blackPrototype
+	case Blue:
+		newShirt = *bluePrototype
+	default:
+		return nil, errors.New(fmt.Sprintf("Unknown color code: %d", s))
+	}
+	return &newShirt, nil
+
 }
 
 type ItemInfoGetter interface {
@@ -35,13 +50,25 @@ type Shirt struct {
 }
 
 func (s *Shirt) GetInfo() string {
-	return ""
+	return fmt.Sprintf("Shirt with SKU '%s' and Color id %d that costs %f\n", s.SKU, s.Color, s.Price)
 }
 
 var whitePrototype *Shirt = &Shirt{
 	Price: 15.00,
 	SKU:   "empty",
 	Color: White,
+}
+
+var blackPrototype *Shirt = &Shirt{
+	Price: 16.00,
+	SKU:   "empty",
+	Color: Black,
+}
+
+var bluePrototype *Shirt = &Shirt{
+	Price: 17.00,
+	SKU:   "empty",
+	Color: Blue,
 }
 
 func (i *Shirt) GetPrice() float32 {
