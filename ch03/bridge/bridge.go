eeefe2536e1fcacb914ed146d13fb57ce5ffb32a
Alejandro Guirao <lekumberri@gmail.com>
2018-10-22 09:40:31 +0200

    Add tests

--- ch03/bridge/bridge.go
+++ ch03/bridge/bridge.go
@@ -1,6 +1,9 @@
 package bridge
 
-import "errors"
+import (
+	"errors"
+	"io"
+)
 
 type PrinterAPI interface {
 	PrintMessage(string) error
@@ -9,5 +12,49 @@ type PrinterAPI interface {
 type PrinterImpl1 struct{}
 
 func (p *PrinterImpl1) PrintMessage(msg string) error {
+	return errors.New("Not implemented yet")
+}
+
+type PrinterImpl2 struct {
+	Writer io.Writer
+}
+
+func (p *PrinterImpl2) PrintMessage(msg string) error {
+	return errors.New("Not implemented yet")
+}
+
+type TestWriter struct {
+	Msg string
+}
+
+func (t *TestWriter) Write(p []byte) (n int, err error) {
+	n = len(p)
+	if n > 0 {
+		t.Msg = string(p)
+		return n, nil
+	}
+	err = errors.New("Content received on Writer was empty")
+	return
+}
+
+type PrinterAbstraction interface {
+	Print() error
+}
+
+type NormalPrinter struct {
+	Msg     string
+	Printer PrinterAPI
+}
+
+func (c *NormalPrinter) Print() error {
+	return errors.New("Not implemented yet")
+}
+
+type PacktPrinter struct {
+	Msg     string
+	Printer PrinterAPI
+}
+
+func (c *PacktPrinter) Print() error {
 	return errors.New("Not implemented yet")
 }
